faaf15ae024a8e3fc6d6e56911c58e381a124535
Viet <salidate96@gmail.com>
2019-05-19 22:35:14 +0300

    Наш четвертый комит

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -1,5 +1,21 @@
-'use strict';
+let money = prompt('Ваш бюджет на месяц?'),
+    date = prompt('Введите дату в формате YYYY-MM-DD');
 
+let appData = {
+    budget: money,
+    timeData: date,
+    expenses: {},
+    optionalExpenses:[],
+    income:[],
+    savings: false
+};
 
+let a1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    a2 = prompt ('Во сколько обойдется?'),
+    a3 = prompt ('Введите обязательную статью расходов в этом месяце'),
+    a4 = prompt ('Во сколько обойдется?');
 
-alert ('Hellow World');
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
